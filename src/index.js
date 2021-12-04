69f560175b333565e05b4bf1b31c1652aac882d8
jere <jnahuel46@gmail.com>
2021-12-04 20:32:59 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+//routes
+app.use('/api/products', require('./routes/products.routes'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
